cb813719538a08fb9774b4fe63aa236cba6cdbf6
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-11-11 10:44:36 -0800

    added layout

---
+++ deaddrop/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script>
+    /** @type {import('./$types').LayoutData} */
+    export let data;
+    // this is where you put your dashboard or basically the things every page has which is the searchbar, nav bar, side bar etc
+</script>
