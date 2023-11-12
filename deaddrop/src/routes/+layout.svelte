cccefea28e8844798922b974fc193a9d1eda8b72
Buslon-Brian <bbuslon@nevada.unr.edu>
2023-11-11 17:09:36 -0800

    Added SearchBar Component and <slot/> to layout

--- deaddrop/src/routes/+layout.svelte
+++ deaddrop/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
     /** @type {import('./$types').LayoutData} */
     export let data;
+    import SearchBar from './SearchBar.svelte';
     // this is where you put your dashboard or basically the things every page has which is the searchbar, nav bar, side bar etc
 </script>
+
+<div>
+	
+	<main>
+		<slot />
+	</main>
+
+</div>
