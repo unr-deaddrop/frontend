26595b37fa725472c735b3a3f8888016adbba69d
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-11-12 16:43:32 -0800

    finish progressbar

--- deaddrop/src/routes/han/tasks/+page.svelte
+++ deaddrop/src/routes/han/tasks/+page.svelte
@@ -2,8 +2,10 @@
     /** @type {import('./$types').PageData} */
     // import Button from '../../../lib/Button.svelte'; // should use button, interactionbutton, progressbar, taskrow, taskrowitem, tasktable
     import Button from '$lib/components/Button.svelte';
+    import ProgressBar from './ProgressBar.svelte';
     export let data;
 </script>
 DeadDrop
+<Button href='./tasks/edit'>Edit</Button>
 <Button>Sample</Button>
-<Button>Sample</Button>
+<ProgressBar progress={.9}></ProgressBar>
--- deaddrop/src/routes/han/tasks/ProgressBar.svelte
+++ deaddrop/src/routes/han/tasks/ProgressBar.svelte
@@ -1,4 +1,41 @@
 <script>
     // progress bar
     // this is a progress bar for the task progress?
+    // bug where whitespace on left side
+    import { tweened } from "svelte/motion";
+    import {cubicOut} from "svelte/easing";
+    const progressTween = tweened(0, {
+        duration: 600,
+        easing: cubicOut
+    });
+
+    export let progress;
+    function updateProgress(){ progressTween.set(progress); } // progress is between 0 and 1
+    updateProgress(); // may not be needed idk
+    $: progress, updateProgress()
+
+
 </script>
+
+
+<div class="outer">
+    <div class="inner" style="--progress: {$progressTween * 100}%"/>
+</div>
+
+<style>
+    .inner, .outer{
+        height: 10px;
+        border-radius: 10px;
+    }
+    .outer{
+        width: 10vw;
+        background-color: transparent;
+        /* margin: 10px auto; */
+        margin: 10px;
+        border: 1px solid #a60707;
+    }
+    .inner{
+        width: var(--progress);
+        background-color: #a60707;
+    }
+</style>
