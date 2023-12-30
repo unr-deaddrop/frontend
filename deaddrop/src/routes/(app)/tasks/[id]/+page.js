c037fa3b335edc19710ca806af970550a968791e
Arellano-Jann <jannrellano@nevada.unr.edu>
2023-12-30 01:12:34 -0800

    fix error on tasks

--- deaddrop/src/routes/(app)/tasks/[id]/+page.js
+++ deaddrop/src/routes/(app)/tasks/[id]/+page.js
@@ -29,14 +29,36 @@ export async function load({params}) {
         },
         method: 'GET'
     });
-    data['task_result'] = await task_result.json(); // where to get
+    data['task_result'] = await task_result.json();
 
-    const logs = await fetch(`http://127.0.0.1:8000/backend/logs/?task_result=${data['task_result'].id}`, {
-        headers: {
-            "Content-Type": "application/json"
-        },
-        method: 'GET'
-    });
-    data['associated_logs'] = await logs.json(); // where to get
+    // needs to loop through list of task_results and fetch each
+    // do this by creating a function and adding to logs
+    // this is needed since task_result may be a list
+    // i.e. multiple task_results to one task (but is this correct?)
+    // even if that's not correct, i don't think the implementation changes anyway
+    async function getLog(task_result) {
+        console.log(task_result.id);
+        let log = await fetch(`http://127.0.0.1:8000/backend/logs/?task_result=${task_result.task}`, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method: 'GET'
+        });
+
+        // logs.push(await log.json())
+        return await log.json();
+    }
+    
+    // data['associated_logs'] = logs; // where to get
+    data['associated_logs'] = await Promise.all(data['task_result'].map(getLog));
+
+    // however, if the above is not true and task_result should not be a list, this implementation should work fine and i'm leaving it here for future use
+    // const logs = await fetch(`http://127.0.0.1:8000/backend/logs/?task_result=${data['task_result'].id}`, {
+    //     headers: {
+    //         "Content-Type": "application/json"
+    //     },
+    //     method: 'GET'
+    // });
+    // data['associated_logs'] = await logs.json(); // where to get
     return data;
 };
--- deaddrop/src/routes/(app)/tasks/[id]/+page.svelte
+++ deaddrop/src/routes/(app)/tasks/[id]/+page.svelte
@@ -2,10 +2,10 @@
     /** @type {import('./$types').PageData} */
     export let data;
     let {id, task, endpoint, user, task_result, associated_logs} = data;
-    console.log(task) // can grab start_time, end_time, in_progress
-    console.log(endpoint) // can grab name, endpoint_id
-    console.log(task_result) // can grab endpoint, user, start_time
-    console.log(associated_logs) // can grab source, timestamp
+    console.log('task', task) // can grab start_time, end_time, in_progress
+    console.log('endpoint', endpoint) // can grab name, endpoint_id
+    console.log('task_result', task_result) // can grab endpoint, user, start_time
+    console.log('associated_logs', associated_logs) // can grab source, timestamp
 
 </script>
 all data
