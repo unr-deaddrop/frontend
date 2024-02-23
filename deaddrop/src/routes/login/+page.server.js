8a9d78a46f1ada221b88bff938a95d6c44447fa9
Buslon-Brian <bbuslon@nevada.unr.edu>
2024-02-23 13:12:21 -0800

    schema & server

---
+++ deaddrop/src/routes/login/+page.server.js
@@ -0,0 +1,13 @@
+/** @type {import('../$types').PageServerLoad} */
+
+import { superValidate } from "sveltekit-superforms/server"
+
+import { fail } from "@sveltejs/kit";
+
+export async function load() {
+    return {};
+};
+
+export const actions = {
+
+}
