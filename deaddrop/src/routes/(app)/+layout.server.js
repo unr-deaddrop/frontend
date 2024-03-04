cd7467590c4ac4d93b8e7be090bdf6f60e19c7fc
Buslon-Brian <bbuslon.aact@gmail.com>
2024-03-04 13:52:27 -0800

    changed logs route server page to import './$types'

--- deaddrop/src/routes/(app)/+layout.server.js
+++ deaddrop/src/routes/(app)/+layout.server.js
@@ -2,6 +2,7 @@ import { redirect } from '@sveltejs/kit';
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({cookies}) {
+    console.log(cookies.get('sessionid'))
     if(cookies.get('sessionid') != '0000'){throw redirect(302, '/login')}
     return {};
 }
