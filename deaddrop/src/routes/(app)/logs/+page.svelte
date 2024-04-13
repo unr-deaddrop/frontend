9c42c965dccfde77f18431d5c7c7753c80ff1282
Keaton Clark <keatonclark2@gmail.com>
2024-04-13 16:38:28 -0700

    #34 pagination, export logs, search

--- deaddrop/src/routes/(app)/logs/+page.svelte
+++ deaddrop/src/routes/(app)/logs/+page.svelte
@@ -1,6 +1,8 @@
 <script>
     import Pagination from '$lib/components/Pagination.svelte';
     import LineChart from '$lib/components/LineChart.svelte';
+
+    /** @type {import('./$types').PageData} */
     export let data
     let {logs, agent_data, server_data} = data.pagedata
     let log_blacklist = ['data']
@@ -38,7 +40,7 @@
                 <span>Logs</span>
             </div> 
             <div class = "tab_content">
-                <Pagination data = {logs} blacklist = {log_blacklist}/>
+                <Pagination data = {logs['data']} pages={logs['pages']} showDownload={true} blacklist = {log_blacklist}/>
             </div>    
         </div>
     </div>
