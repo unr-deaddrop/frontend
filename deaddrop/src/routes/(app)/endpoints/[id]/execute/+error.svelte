bc9e7ebd20b8658a658125723267caa0f814b6fb
Buslon-Brian <buslon.aact@gmail.com>
2024-04-26 23:12:44 -0700

    Error page for incorrect id

---
+++ deaddrop/src/routes/(app)/endpoints/[id]/execute/+error.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { page } from '$app/stores';
+    
+    export let data;
+    let {endpoint_id, cmd_list, cmd_options, agent_commands} = data;
+</script>
+
+
+<div class = "container">
+    {#if endpoint_id == undefined}
+        <h1>Error: Endpoint ID not found</h1>
+        
+        {:else}
+        <h1>{$page.status}: {$page.error.message}</h1>
+    {/if}
+</div>
+
+<style>
+    h1 {
+        margin-left: 10px;
+    }
+</style>
+
+
